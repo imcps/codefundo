e7236e92bef249b3172359ab15cc4151da8c7be0
IshulJain <ishuljain005@gmail.com>
2017-12-28 23:44:01 +0530

    login/signup View

--- app/login/urls.py
+++ app/login/urls.py
@@ -6,4 +6,6 @@ app_name = 'login'
 
 urlpatterns = [
 	url(r'^$', login, name='login'),
+	url(r'^login/$', loginView, name='loginView'),
+	url(r'^register/$', registerView, name='registerView'),
 ]
--- app/login/views.py
+++ app/login/views.py
@@ -1,5 +1,41 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 # Create your views here.
 def login(request):
 	return render(request, 'login.html')
+
+def registerView(request):
+	if request.method == 'POST':
+		post = request.POST
+
+		email = post.get('email')
+		password1 = post.get('pass')
+		password2 = post.get('cpass')
+		username = getUsername(email)
+
+		if password1 != password2:
+			message.warning(request, "passwords do not match", fail_silently=True)
+			return render(request, 'login.html')
+
+    	user = User.objects.create_user(username=username, email=email)
+    	user.set_password(password1)
+    	user.save()
+    	return HttpResponse('<h1>SUCCESS !!</h1>')
+
+def loginView(request):
+	if request.method == 'POST':
+		post = request.POST
+		email = post['email']
+		password = post['pass']
+		username = getUsername(email)
+		user = authenticate(username=username, email=email, password=password)
+
+		if user is not None:
+			return HttpResponse('<h1>login Success</h1>')
+
+
+def getUsername(email):
+	return "user"
