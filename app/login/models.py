0d3341d23ace384f50fa043688d15ee100a940b8
IshulJain <ishuljain005@gmail.com>
2017-12-29 16:55:42 +0530

    Advertisment model

--- app/login/models.py
+++ app/login/models.py
@@ -1,5 +1,24 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+
+
+categories = [
+        (1, 'Books'),
+        (2, 'Vehicle'),
+    ]
 
 # Create your models here.
+class Advertisment(models.Model):
+	user = models.OneToOneField(User)
+	name = models.CharField(max_length=50)
+	title = models.CharField(max_length=100)
+	category = models.integerField(categories)
+	description = models.CharField(max_length=500)
+	mobileNumber = models.BigIntegerField()
+	address = models.CharField(max_length=250)
+
+
+
+
